ad2c175edd5b6b12309c85d89a107d972f6af7fa
Maks <moskalenko.maksim1990@gmail.com>
2022-05-17 18:42:45 +0300

    add score counter

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const dino = document.getElementById("dino"),
-      cactus = document.getElementById("cactus");
+      cactus = document.getElementById("cactus"),
+      elHiScore = document.querySelector(".hiScore"),
+      elScore = document.querySelector(".score");
+let myScore = 0,
+    hiScore = localStorage.getItem("hiScore");
+    if (hiScore===undefined){
+        hiScore = 0;
+        localStorage.setItem("hiScore",0);
+    }
     
+elHiScore.textContent = hiScore; 
+
 document.addEventListener('keydown', function(){
     jump();
 });
@@ -19,7 +29,16 @@ let isAlive = setInterval(() => {
    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
     
-   if (cactusLeft<50 && cactusLeft>0 && dinoTop >=150){
-       alert("Game Over!!!");
+   if (cactusLeft<50 && cactusLeft>0 && dinoTop >=168){
+    alert("Game Over!!!");
+    if (myScore > hiScore){
+        localStorage.setItem("hiScore",myScore); 
+        history = myScore; 
+        elHiScore.textContent = hiScore; 
+    }
+        myScore = 0;
+   } else{
+    myScore +=1;
    }
+   elScore.textContent = myScore; 
 }, 10);
